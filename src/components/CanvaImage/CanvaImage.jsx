06933493344185acdf2ce5ab926df0ce9a707264
chao lobi <gogoiladitya@gmail.com>
2023-07-07 17:33:15 +0530

    dummy

--- src/components/CanvaImage/CanvaImage.jsx
+++ src/components/CanvaImage/CanvaImage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import homeimage from '../../assets/homeimage.png';
+import homeImage from '../../assets/homeImage.png'
 // styles
 import classes from './CanvaImage.module.css';
 
@@ -7,7 +7,7 @@ const CanvaImage = () => {
   return (
     <div className={classes.canvaimage}>
       <div className={classes.imgcontainer}>
-          <img className={classes.homeimage} src={homeimage} alt="home-image" />
+          <img className={classes.homeimage} src={homeImage} alt="home" />
       </div>
         
     </div>
